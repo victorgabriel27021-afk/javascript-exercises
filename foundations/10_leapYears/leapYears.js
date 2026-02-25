2f3ebd38deb864115d5eba7d9de97a92e8ab5f90
Victor Gabriel <victorgabriel27021@gmail.com>
2026-02-25 15:33:34 -0300

    Javascriot exercise - 10 - valid

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,4 +1,15 @@
-const leapYears = function() {
+const leapYears = function(ano) {
+	function ehbissexto(ano){
+		if (ano % 4 === 0)
+			return true;
+	}
+	    if (ano % 100 === 0) {
+	    	return false;
+	}
+	    if ( ano % 4 === 0) {
+	    	return true;
+	}
+	return false;
 
 };
 
