139310c748c831078c982db1321ff10018507a06
Victor Gabriel <victorgabriel27021@gmail.com>
2026-02-25 15:26:34 -0300

    Javascriot exercise - 09 - valid

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(soma) {
+	soma(3, 4)
+	let soma = 0
+	let inicio = Math.min(3, 4)
+	let fim = Math.max(3, 4)
+	for (let i = inicio; i <= fim; i++){
+		soma += i
+	}
+    return soma;
 };
 
 // Do not edit below this line
