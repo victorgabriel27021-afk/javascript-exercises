e3f2e2becedfd8406529b5b7c25d6ff4643fab2c
Victor Gabriel <victorgabriel27021@gmail.com>
2026-02-25 15:15:10 -0300

    Javascriot exercise - 08 - valid

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+ const removeFromArray = function(array, ...args) {
+ 	 const newArray = [];
+ 	 array.forEach((item) => {
+ 	 	f (!args.includes(item)) {
+      newArray.push(item);
+    }
+  });
+ 	 return newArray;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
