2c5d007830422056870a2d1dac5549840fa550fc
Victor Gabriel <victorgabriel27021@gmail.com>
2026-02-25 14:34:20 -0300

    Javascript exercise - 06 - valid

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (Vt, Tv) {
+if (Tv < 0) return "ERROR";
+let string ""
+for (let i = 0; i < Tv; i++){
+	string += Vt
+}
+return string;
 };
 
 // Do not edit below this line
