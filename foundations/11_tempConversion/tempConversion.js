c58630f71e81e2851568ed891d9291299457f83e
Victor Gabriel <victorgabriel27021@gmail.com>
2026-02-26 09:56:26 -0300

    Javascript exercise  - 11 - valid

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
 const convertToCelsius = function() {
+  function fahrenheitToCelsius (fahrenheit)
+  return (fahrenheit - 32) * 5 / 9;
 };
 
 const convertToFahrenheit = function() {
+  function celciusToFarenheit (celsius)
+  return celsius * 9 / 5 + 32;
 };
 
 // Do not edit below this line
