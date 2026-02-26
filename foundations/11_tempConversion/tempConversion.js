c5a261e7bff4160f20fb52013f3a66ee7682748d
Victor Gabriel <victorgabriel27021@gmail.com>
2026-02-26 10:04:35 -0300

    Javascriot exercise - 11  - correction

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,11 @@
 const convertToCelsius = function() {
   function fahrenheitToCelsius (fahrenheit)
-  return (fahrenheit - 32) * 5 / 9;
+  return Math.round(((fahrenheit - 32) * 5 / 9) * 10 ) / 10;
 };
 
 const convertToFahrenheit = function() {
   function celciusToFarenheit (celsius)
-  return celsius * 9 / 5 + 32;
+  return Math.round ((celsius * 9 / 5 + 32) * 10 ) / 10;
 };
 
 // Do not edit below this line
