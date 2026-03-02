2113b61dac8f003db9d75a6db1a1a3c1d0348eb0
Victor Gabriel <victorgabriel27021@gmail.com>
2026-03-02 12:20:23 -0300

    Palindromes simple version

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+	const reversedString = str.split("").reverse().join("")
+	return str === reversedString
 };
 
+console.log (ispalindrome("Ana")); //true
+console.log (ispalindrome("hello"));  //false
 // Do not edit below this line
 module.exports = palindromes;
