8f6a3ba53956d2f04441619555bffed68c7f7549
Victor Gabriel <victorgabriel27021@gmail.com>
2026-03-02 13:21:21 -0300

    findTheOldest.js simple version

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  const currentYearn= new Date ().getFullYear();
+  return people.reduce((oldest, person) => {
+  	const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+  	const personAge = (person.yearOfDeath || currentYear) - oldest.yearOfBirth;
+  	return personAge > oldestAge ? person : oldest;
+  });
 };
 
 // Do not edit below this line
