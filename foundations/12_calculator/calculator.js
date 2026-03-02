f4cac4d0483131db3b3f0fb06ff304e333754caf
Victor Gabriel <victorgabriel27021@gmail.com>
2026-03-02 12:10:47 -0300

    calculater.js valid

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return = a + b;
+	console.log (add(1, 2));
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return = a - b;
+	console.log (substract(3, 4));
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, current) => total + current, 0)
+	console.log(sum([1, 2, 3, 4]));
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	return array.reduce ((product, current) => product * current, 1)
+	console.log(multiply([1, 2, 3, 4]));
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return = a ** b;
+  console.log (add(3, 4))
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n < 0) "Negative's number don't have factorial";
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+  	product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
