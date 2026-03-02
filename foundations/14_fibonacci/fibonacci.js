365b2039adcc428eb5c9dc076a5d48b21d34ae20
Victor Gabriel <victorgabriel27021@gmail.com>
2026-03-02 12:35:06 -0300

    Fibonacci simple version

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+	if (n < 0) return "invalid input"
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+    let prev = 0;
+    let current = 1;
+    for (let i = 2; i <= n; i++){
+    	let next = prev + current;
+    	prev = current;
+    	current = next;
+    }
 
+    return current;
 };
 
 // Do not edit below this line
